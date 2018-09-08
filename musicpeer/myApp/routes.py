7bf99d7fde1da08ee5554979cfa8b41b43419787
aayushagrawal135 <aayushagrawal135@gmail.com>
2018-09-08 22:51:35 +0530

    Adding html: beginner

--- musicpeer/myApp/routes.py
+++ musicpeer/myApp/routes.py
@@ -1,6 +1,8 @@
 from myApp import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Aayush'}
+    return render_template('index.html', user = user)
