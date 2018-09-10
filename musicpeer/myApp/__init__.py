f4b84d3eeb84a9f6006a22399b159a4409575bb9
aayushagrawal135 <aayushagrawal135@gmail.com>
2018-09-10 12:34:59 +0530

    Url maps updated, email in

--- musicpeer/myApp/__init__.py
+++ musicpeer/myApp/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# creating an application instance
 app = Flask(__name__)
+# adding the config file to the application
 app.config.from_object(Config)
-
+# creating a database instance
+db = SQLAlchemy(app)
+# creating a migration engine instance
+migrate = Migrate(app, db)
+# routes has all view functions; models help to define the structure of the database
 from myApp import routes
